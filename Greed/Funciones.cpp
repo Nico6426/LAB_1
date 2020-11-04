9afd34e1c2886fae2bf071f025f94ba8914323c8
Nico <dececconicolas@gmail.com>
2020-11-03 21:52:38 -0300

    Creacion proyecto GREED con main y funciones

---
+++ Greed/Funciones.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+void Menu()
+{
+      setlocale(LC_ALL, "Spanish");
+
+    cout<<"-------------------------------"<<endl<<endl;
+    cout<<"Bienvenido a GREED!"<<endl<<endl;
+    cout<<"-------------------------------"<<endl<<endl;
+    system("pause");
+    system("cls");
+    cout<<"Menu principal"<<endl<<"-------------------------------"<<endl;
+    cout<<"1) Modo UN jugador "<<endl;
+    cout<<"2) Modo DOS jugadores "<<endl;
+    cout<<"3) Estadísticas"<<endl;
+    cout<<"4) INSTRUCCIONES "<<endl;
+    cout<<"5) Créditos "<<endl<<"-------------------------------"<<endl;
+    cout<<"6) Salir "<<endl;
+
+
+
+}
