84a03a81c2aa37a9ccd95f730323e352ccaab789
EduardoChavero <chave.11@live.com.ar>
2020-11-11 02:50:46 -0300

    Agregando funciones
    Funcion jugador, cumple la logica. Falta estetica.

--- Greed/Funciones.h
+++ Greed/Funciones.h
@@ -3,5 +3,8 @@
 void menu(int [], char [], char[]);
 void dados(int [], int);
 void jugador1(int [], char []);
-
+void mostrar(int [], int);
+int sumar(int [], int);
+int eliminar(int [], int, int [], int);
+void ponerCero(int [], int);
 
